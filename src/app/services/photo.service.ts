f1a5a7738e41bd28797818e187e6763e32f37e23
LeoAvram <leo.avramovic@gmail.com>
2021-08-28 21:28:38 +0200

    Displaying Photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Storage } from '@capacitor/storage';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
+
+  public photos: Photo[] = [];
 
   constructor() { }
 
@@ -17,6 +18,14 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
+
+    this.photos.unshift({
+      filepath: 'soon...',
+      webviewPath: capturedPhoto.webPath
+    });
   }
 }
-
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
+}
