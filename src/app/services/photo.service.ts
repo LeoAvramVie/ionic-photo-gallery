54f360d7d9205def105d48a04ce1da0729c7dc8f
ATAVRAMO_Z <ziva.avramovic@twinfaktor.at>
2021-09-04 16:39:17 +0200

    Loading Photos from the Filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Camera, CameraPhoto, CameraResultType, CameraSource} from '@capacitor/camera';
-import {Directory, Filesystem} from "@capacitor/filesystem";
+import {Directory, Filesystem} from '@capacitor/filesystem';
+import {Storage} from '@capacitor/storage';
 
 
 @Injectable({
@@ -9,21 +10,45 @@ import {Directory, Filesystem} from "@capacitor/filesystem";
 export class PhotoService {
 
   public photos: Photo[] = [];
-
-  constructor() {
-  }
+  private PHOTO_STORAGE = `photos`;
 
   public async addNewToGallery() {
-    // Take a photo
+// Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     });
 
+
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
+
+
+    await Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+    console.log('Bin aufgerufen worden ', savedImageFile);
+
+  }
+
+  public async loadSaved(){
+    // Retrieve chaed photo array data
+    const photoList = await Storage.get({key: this.PHOTO_STORAGE});
+    this.photos = JSON.parse(photoList.value) || [];
+
+    //Display the photo by reding into base64 format
+    for (const photo of this.photos){
+      //Read each saved photo´s data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data
+      });
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+
   }
 
   private async savePicture(cameraPhoto: CameraPhoto) {
@@ -32,7 +57,7 @@ export class PhotoService {
 
     // Write the file to the data directory
     const fileName = new Date().getTime() + '.jpeg';
-    const savedFile = await Filesystem.writeFile({
+    await Filesystem.writeFile({
       path: fileName,
       data: base64Data,
       directory: Directory.Data
@@ -47,20 +72,24 @@ export class PhotoService {
 
   private async readAsBase64(cameraPhoto: CameraPhoto) {
     // Fetch the photo, read as a blob, then convert to base64 format
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const response = await fetch(cameraPhoto.webPath!);
     const blob = await response.blob();
 
     return await this.convertBlobToBase64(blob) as string;
   }
 
-  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
-    const reader = new FileReader;
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
     reader.onerror = reject;
     reader.onload = () => {
       resolve(reader.result);
     };
     reader.readAsDataURL(blob);
   });
+
+
+
 }
 
 export interface Photo {
